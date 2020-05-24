357035e88adb5e014d99c3f73966a2a4d647cc69
gkpty <gkardonski@gmail.com>
2020-05-24 16:14:19 -0500

    added the audit reoport recommendations section. still need to add a couple of unconventional results

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 const express = require('express')
 const app = express()
+const recommendations = require('./recommendations')
+
+const threshhold = {
+  unacceptable: .7,
+  acceptable: .8,
+  good: .9,
+  recommendations: .7
+}
 
 function launchChromeAndRunLighthouse(url, opts, config = null) {
   return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
@@ -16,10 +24,10 @@ const opts = {
   chromeFlags: ['--show-paint-rects', '--headless']
 };
 
-function main(index, port){
+function main(dir, index, port, threshhold){
   return new Promise((resolve, reject)=> {
-    app.use(express.static('output'))
-    var server = app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+    app.use(express.static(dir))
+    var server = app.listen(port, () => console.log(`serving static site located at ${dir} in port ${port}`))
     let fileurl = 'http://localhost:8080/'+index
     launchChromeAndRunLighthouse(fileurl, opts).then(results => {
       //format audits then return formatted audits
@@ -35,8 +43,73 @@ function main(index, port){
       //lighthouse 6 score
       formatted.lh6_score = .15*audit["first-contentful-paint"].score + .15*audit["speed-index"].score + .25*audit["largest-contentful-paint"].score + .15*audit["interactive"].score + .25*audit["total-blocking-time"].score +.05*audit["cumulative-layout-shift"].score;
       //main metrics
-      formatted.main_metrics["first-contentful-paint"] = audit["first-contentful-paint"]
+      formatted.main_metrics["first-contentful-paint"] = {
+        title: audit["first-contentful-paint"].title,
+        score: audit["first-contentful-paint"].score,
+        description: audit["first-contentful-paint"].description
+      }
+      formatted.main_metrics["speed-index"] = {
+        title: audit["speed-index"].title,
+        score: audit["speed-index"].score,
+        description: audit["speed-index"].description
+      }
+      formatted.main_metrics["largest-contentful-paint"] = {
+        title: audit["largest-contentful-paint"].title,
+        score: audit["largest-contentful-paint"].score,
+        description: audit["largest-contentful-paint"].description
+      }
+      formatted.main_metrics["total-blocking-time"] = {
+        title: audit["total-blocking-time"].title,
+        score: audit["total-blocking-time"].score,
+        description: audit["total-blocking-time"].description
+      }
+      formatted.main_metrics["interactive"] = {
+        title: audit["interactive"].title,
+        score: audit["interactive"].score,
+        description: audit["interactive"].description
+      }
+      formatted.main_metrics["cumulative-layout-shift"] = {
+        title: audit["cumulative-layout-shift"].title,
+        score: audit["cumulative-layout-shift"].score,
+        description: audit["cumulative-layout-shift"].description
+      }
+      formatted.main_metrics["first-meaningful-paint"] = {
+        title: audit["first-meaningful-paint"].title,
+        score: audit["first-meaningful-paint"].score,
+        description: audit["first-meaningful-paint"].description
+      }
+      formatted.main_metrics["first-cpu-idle"] = {
+        title: audit["first-cpu-idle"].title,
+        score: audit["first-cpu-idle"].score,
+        description: audit["first-cpu-idle"].description
+      }
       //recommendations
+      for(let r of recommendations){
+        switch(audit[r].scoreDisplayMode){
+          case 'binary':
+            if(audit[r].score === 0){
+              formatted.recommendations[r] = {
+                title: audit[r].title,
+                score: audit[r].score,
+                description: audit[r].description,
+              }
+              if(audit[r].details) formatted.recommendations[r].details = audit[r].details
+            }
+            break;
+          case 'numeric':
+            if(audit[r].score < threshhold.recommendations){
+              formatted.recommendations[r] = {
+                title: audit[r].title,
+                score: audit[r].score,
+                description: audit[r].description,
+              }
+              if(audit[r].details) formatted.recommendations[r].details = audit[r].details
+            }
+            break;
+          default:
+            console.log(`Error scoreDisplayMode ${audit[r].scoreDisplayMode} not recognized`)
+        }
+      }
       server.close('Closed server in port ' + port)
       resolve(formatted)
     }).catch(err => {
@@ -46,4 +119,4 @@ function main(index, port){
   })
 }
 
-main('index.html', 8080).then(data=> console.log(data)).catch(err=>console.log(err))
+main('output', 'index.html', 8080, threshhold).then(data=> console.log(data)).catch(err=>console.log(err))
